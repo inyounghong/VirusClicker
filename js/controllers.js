aa11a04d37d77f8d26a68593045587b2060f6e59
Inyoung Hong <ih235@cornell.edu>
2015-08-19 19:20:08 -0700

    Fixed spokes

--- js/controllers.js
+++ js/controllers.js
@@ -3,12 +3,12 @@ app.controller('MainController', function($scope, $http){
 	$scope.game = { score: 0, growth: 0.2 };
 
 	$scope.microbes = [
-		{"name": "Extra Spoke", 		"cost": 10, "count":0, "growth": 0.2, 	"spoke": 'spoke1', "req": 0},
-		{"name": "Advanced Spoke", 		"cost": 20, "count":0, "growth": 0.4, 	"spoke": 'spoke2', "req": 0.6},
-		{"name": "Cool Spoke", 			"cost": 100, "count":0, "growth": 1, 	"spoke": 'spoke3', "req": 1.2},
-		{"name": "Super Spoke", 		"cost": 500, "count":0, "growth": 5, 	"spoke": 'spoke4', "req": 5},
-		{"name": "Super Ultra Spoke", 	"cost": 3000, "count":0, "growth": 10, 	"spoke": 'spoke5', "req": 10},
-		{"name": "Ultra Mega Spoke", 	"cost": 10000, "count":0, "growth": 25, 	"spoke": 'spoke6', "req": 25},
+		{"name": "Extra Spoke", 		"cost": 10, 	"count":0, "growth": 0.2, 	"spoke": 'spoke1', "req": 0},
+		{"name": "Advanced Spoke", 		"cost": 20, 	"count":0, "growth": 0.5, 	"spoke": 'spoke2', "req": 0.6},
+		{"name": "Cool Spoke", 			"cost": 100, 	"count":0, "growth": 2, 	"spoke": 'spoke3', "req": 2.0},
+		{"name": "Super Spoke", 		"cost": 500, 	"count":0, "growth": 8, 	"spoke": 'spoke4', "req": 8},
+		{"name": "Super Ultra Spoke", 	"cost": 2000, 	"count":0, "growth": 14, 	"spoke": 'spoke5', "req": 24},
+		{"name": "Ultra Mega Spoke", 	"cost": 10000, 	"count":0, "growth": 25, 	"spoke": 'spoke6', "req": 55},
 	]
 
 	function addScore(amount){
